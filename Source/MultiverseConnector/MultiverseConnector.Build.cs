e8ea64017e6a9b19492f6d4c83aa3b3017f2d2e0
Giang Nguyen <hoanggia@uni-bremen.de>
2023-07-06 16:58:25 +0200

    Add allow exception

--- Source/MultiverseConnector/MultiverseConnector.Build.cs
+++ Source/MultiverseConnector/MultiverseConnector.Build.cs
@@ -35,5 +35,7 @@ public class MultiverseConnector : ModuleRules
     // PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
     // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+    bEnableExceptions = true;
   }
 }
