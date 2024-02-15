a65cd60be998d46a5fe1b4d10835eb8ca39904fa
Giang Hoang Nguyen <hoanggia@uni-bremen.de>
2024-02-15 21:41:03 +0100

    Fix funny bug

--- Source/ThirdParty/MultiverseClientLibrary/MultiverseClientLibrary.Build.cs
+++ Source/ThirdParty/MultiverseClientLibrary/MultiverseClientLibrary.Build.cs
@@ -16,26 +16,22 @@ public class MultiverseClientLibrary : ModuleRules
 
 		if (Target.Platform == UnrealTargetPlatform.Linux)
 		{
-			string MultiverseClientSoPath = Path.Combine("$(PluginDir)", "Binaries", "ThirdParty", 
-				"MultiverseClientLibrary", "Linux", "libmultiverse_client.so");
+			string MultiverseClientSoPath = Path.Combine("$(PluginDir)", "Binaries", "Linux", "libmultiverse_client.so");
 			PublicAdditionalLibraries.Add(MultiverseClientSoPath);
 			PublicDelayLoadDLLs.Add(MultiverseClientSoPath);
 			RuntimeDependencies.Add(MultiverseClientSoPath);
 		}
 		else if (Target.Platform == UnrealTargetPlatform.Win64)
 		{
-			string MultiverseClientLibPath = Path.Combine("$(PluginDir)", "Binaries", "ThirdParty",
-				"MultiverseClientLibrary", "Win64", "multiverse_client.lib");
+			string MultiverseClientLibPath = Path.Combine("$(PluginDir)", "Binaries", "Win64", "multiverse_client.lib");
 			PublicAdditionalLibraries.Add(MultiverseClientLibPath);
 			
-			string ZmqLibPath = Path.Combine("$(PluginDir)", "Binaries", "ThirdParty",
-				"MultiverseClientLibrary", "Win64", "libzmq-mt-4_3_5.lib");
+			string ZmqLibPath = Path.Combine("$(PluginDir)", "Binaries", "Win64", "libzmq-mt-4_3_5.lib");
 			PublicAdditionalLibraries.Add(ZmqLibPath);
 		}
 		else if (Target.Platform == UnrealTargetPlatform.Android)
 		{
-			string MultiverseClientLibPath = Path.Combine("$(PluginDir)", "Binaries", "ThirdParty", 
-				"MultiverseClientLibrary", "Android", "libmultiverse_client.so");
+			string MultiverseClientLibPath = Path.Combine("$(PluginDir)", "Binaries", "Android", "libmultiverse_client.so");
 			PublicAdditionalLibraries.Add(MultiverseClientLibPath);
 		}
 	}
